ee3c65d77099828c3a2890ed283144fa113cda54
Amal221200 <amalmurikkoli2000@gmail.com>
2022-02-06 20:56:47 +0530

    Some improvements

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -13,16 +13,57 @@ const TextForm = ({ heading }) => {
         setText(newText);
     }
 
+    const handleLoClick = () =>{
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+
+    // let str = "Hello ";
+
+    // console.log(str.lastIndexOf(""))
+    const numberOfWords = (text) =>{
+        let number = 0;
+
+        if(text === ""){
+            number = 0;
+        }else if(text.lastIndexOf(" ") === (text.length - 1)){
+            number = text.split(" ").length - 1;
+        }else{
+            number = text.split(" ").length;
+        }
+
+        return number;
+    }
+
+    const backSpace = () =>{
+        const newText = text.slice(0, text.length - 1)
+        setText(newText);
+    }
+
 
   return (
-    <div>
-        <h1>{heading}</h1>
+      <>
+    <div className='container'>
+        <h2>{heading}</h2>
         <div className="mb-3">
             <textarea className="form-control" placeholder="Enter your text" value={text} onChange={handleOnClick} id="floatingTextarea" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>Convert To Uppercase</button>
+        <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert To Uppercase</button>
+        <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert To Lowercase</button>
+        <button className="btn btn-primary mx-1" onClick={backSpace}>Backspace</button>
+        <button className="btn btn-primary mx-1" onClick={() => setText("")}>Clear</button>
     </div>
+    <div className="container">
+        <h2>Text Summary</h2>
+        <p>{numberOfWords(text)} words and {text.length} characters</p>
+        <p>{0.008 * (numberOfWords(text))} minutes to read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+    </div>
+    </>
   );
 };
+
+
 
 export default TextForm;
